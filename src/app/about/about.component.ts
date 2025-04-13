cd229995c0a4b51a9351c91428f56215a20d981f
Lobna HAriz <lobnahariz@gmail.com>
2025-04-13 16:10:55 +0200

    ajout code

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../services/api/products/product.service";
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {MyFirstCompComponent} from "./my-first-comp/my-first-comp.component";
+import {AboutComponent} from "./about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: MyFirstCompComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'about/:username',
+    component: AboutComponent
+  }
+];
